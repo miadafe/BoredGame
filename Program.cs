c20fffd16ca9c36daabf2170487a07fd61b5d590
mia dafe <miadafe@mias-MacBook-Air-2.local>
2025-03-02 14:57:03 +0000

    Quiz class

--- Program.cs
+++ Program.cs
@@ -3,83 +3,119 @@
 
 namespace BoredGame{
 
-    class Timer{
+    static class GlobalStore{
+        public static ConsoleKeyInfo keyPress;
+        public static string selectedColour = "\u001b[32m";
+        public static string escapeColour = "\u001b[0m";
+        public static int score = 0;
+    }
 
+    class Timer{
         public static DateTime getTime(){
             return DateTime.Now;
         }
-
-
-
         public static TimeSpan timeDiffT(DateTime start, DateTime end){    
             return end.Subtract(start);
+        }
+    }
+
+    class Quiz{
+        string[] questions = {"How was your weekend?", "What's your best talent?", "Who is your icon?"};
+        string[,] answers = {{"awful...", "decent", "the best"}, {"My eloquence", "My athleticism", "My candour"}, {"Ru-Paul", "Myself", "Nobody"}};
+        int[] correctAns = [2, 0, 0];
+        int choices = 3;
+
+        public void DisplayScore(){
+            Console.WriteLine($"You've earned a score of {GlobalStore.score}");
+        }
+
+        public void DisplayQuestion(int question){
+            Console.WriteLine(questions[question]);
+        }
+    
+
+        public void DisplayAnswers(int question){
+            for (int ans = 0; ans < 3; ans++){
+                    Console.WriteLine(answers[question, ans]);
+                }
+        }
+
+        public void DisplayAnswersWithUserInput(int question){
+            int selectedOption = 0;
+            Boolean thinkingOfAnswer = true;
+            //after asking the question, get the cursor position
+            (int x, int y) = Console.GetCursorPosition();
+
+
+            while (thinkingOfAnswer){
+                Console.SetCursorPosition(x, y);
+                for (int i = 0; i < choices; i++){
+                    if (selectedOption == i){
+                        Console.WriteLine(GlobalStore.selectedColour + answers[question, i] + GlobalStore.escapeColour);
+                    }
+                    else Console.WriteLine(answers[question, i] + GlobalStore.escapeColour);
+                }
+
+                GlobalStore.keyPress = Console.ReadKey(true);
+
+                switch (GlobalStore.keyPress.Key){
+                    case ConsoleKey.DownArrow:
+                        selectedOption++;
+                        if(selectedOption > 2){
+                            selectedOption = 0;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        selectedOption--;
+                        if(selectedOption < 0){
+                            selectedOption = 2;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        thinkingOfAnswer = false;
+                        break;
+                }
+            } 
+
+            if(selectedOption == correctAns[question]){
+                    GlobalStore.score++;
+            }
+
         }
     }
 
     class Program{
 
         static void Main(string[] args){
-
-            bool running = true;
             Program prog = new Program();
 
-            Console.WriteLine("i heard you're bored?\n");
-
+            Console.WriteLine("I heard you're bored?\n");
 
             DateTime startTime = Timer.getTime();
-            Boolean asking = true;
-            string[] options = ["yes i am", "no im not", "who's asking"];
-            string[] answers = ["bored", "fine", "suspicious"];
-            int userOption = 0;
-            string selectedColour = "\u001b[32m";
-            string escapeColour = "\u001b[0m";
             (int x, int y) = Console.GetCursorPosition();
-            ConsoleKeyInfo keyPress;
-
-            while (asking){
-
-                Console.SetCursorPosition(x, y);
-
-                for (int i = 0; i < options.Length; i++){
-                    if (userOption == i){
-                        Console.WriteLine(selectedColour + options[i] + escapeColour);
-                    }
-                    else Console.WriteLine(options[i] + escapeColour);
-                }
-
-                keyPress = Console.ReadKey(true);
-
-                switch (keyPress.Key){
-                    case ConsoleKey.DownArrow:
-                        userOption++;
-                        if(userOption > 2){
-                            userOption = 0;
-                        }
-                        break;
-                    case ConsoleKey.UpArrow:
-                        userOption--;
-                        if(userOption < 0){
-                            userOption = 2;
-                        }
-                        break;
-                    case ConsoleKey.Enter:
-                        asking = false;
-                        break;
-                }
-
-            }
-
-            Console.WriteLine($"\nyou are {answers[userOption]}");
+            Boolean startQuiz = true;
+            Quiz myQuiz = new Quiz();
 
             DateTime endTime = Timer.getTime();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-            Console.WriteLine($"it took you {Timer.timeDiffT(startTime, endTime).ToString(@"mm\:ss\.ff")} to answer...");
+            Console.WriteLine($"Game data took {Timer.timeDiffT(startTime, endTime).ToString(@"mm\:ss\.ff")} to load...");
 
             Console.ResetColor();
+           
 
-            Console.WriteLine("now what?");
+            while (startQuiz){
+                for (int quizQuestion = 0; quizQuestion < 3; quizQuestion ++){
+                    myQuiz.DisplayQuestion(quizQuestion);
+
+                    myQuiz.DisplayAnswersWithUserInput(quizQuestion);
+
+                }
+                startQuiz = false;
+            }
+
+            myQuiz.DisplayScore();
 
         }
 
